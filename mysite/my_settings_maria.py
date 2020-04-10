cf346b584544876b3608299de7687371fb0ebea2
ek12mv2 <dmsrkd1216@gmail.com>
2020-04-11 01:38:12 +0900

    django tutorial

---
+++ mysite/my_settings_maria.py
@@ -0,0 +1,10 @@
+DATABASES={
+ 'default':{
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'test',
+    'USER': 'root',
+    'PASSWORD': '1234',
+    'HOST': 'localhost',
+    'PORT': '3307',
+ }
+}
